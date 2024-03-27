b643f5a840ed7531145064e42b2a06259be6e17d
AlejandroV <2002433@utrivieramaya.edu.mx>
2024-03-27 16:49:22 -0500

    feat

--- src/app/foods/shared/food.service.ts
+++ src/app/foods/shared/food.service.ts
@@ -1,67 +1,102 @@
-import {Injectable} from '@angular/core';
-import {Food} from './food.model';
-import {CategoryEnum} from './category.enum';
+import { Injectable } from '@angular/core';
+import { Food } from './food.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
-  menu: Food[] = [
+  menu: Food[]=[
     {
-      id: 1,
-      name: 'FOCACCIA',
-      description: 'FOCACCIA Queso',
-      category: CategoryEnum.food,
-      image: 'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
-      price: 234,
+      id:1,
+      name:'FOCACCIA',
+      descripcion:'Con extra queso',
+      category: 'food',
+      image:'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
+      price: 170
     },
     {
-      id: 2,
-      name: 'FOCACCIA',
-      description: 'FOCACCIA Queso',
-      category: CategoryEnum.food,
-      image: 'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
-      price: 234,
+      id:2,
+      name:'FOCACCIA',
+      descripcion:'Con extra queso',
+      category: 'food',
+      image:'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
+      price: 170
     },
     {
-      id: 3,
-      name: 'FOCACCIA',
-      description: 'FOCACCIA Queso',
-      category: CategoryEnum.food,
-      image: 'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
-      price: 234,
+      id:3,
+      name:'FOCACCIA',
+      descripcion:'Con extra queso',
+      category: 'food',
+      image:'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
+      price: 170
     },
     {
-      id: 4,
-      name: 'FOCACCIA',
-      description: 'FOCACCIA Queso',
-      category: CategoryEnum.food,
-      image: 'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
-      price: 234,
+      id:4,
+      name:'FOCACCIA',
+      descripcion:'Con extra queso',
+      category: 'food',
+      image:'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
+      price: 170
     },
     {
-      id: 5,
-      name: 'FOCACCIA ',
-      description: 'FOCACCIA Queso',
-      category: CategoryEnum.food,
-      image: 'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
-      price: 234,
+      id:5,
+      name:'FOCACCIA',
+      descripcion:'Con extra queso',
+      category: 'food',
+      image:'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
+      price: 170
     },
     {
-      id: 6,
-      name: 'FOCACCIA ',
-      description: 'FOCACCIA Queso',
-      category: CategoryEnum.food,
-      image: 'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
-      price: 234,
+      id:6,
+      name:'FOCACCIA',
+      descripcion:'Con extra queso',
+      category: 'food',
+      image:'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
+      price: 170
     },
-  ];
+    {
+      id:7,
+      name:'FOCACCIA',
+      descripcion:'Con extra queso',
+      category: 'food',
+      image:'https://2trendies.com/hero/2023/09/focaccia.jpg?width=1200&aspect_ratio=16:9',
+      price: 150
+    }
+  ]
 
-  constructor() {
-  }
+  constructor() { }
 
-  public getAllFood(): Food[] {
+  public getAllFoods():Food[]{
     return this.menu;
-  }
+  };
+
+  //Obtener una comida del arreglo
+  public getOne(id:number):Food | undefined{
+    return this.menu.find(item=>item.id == id);
+  };
+
+  //Añadir nueva comida
+  public addFood(food:Food) {
+    this.menu.push(food);
+  };
+
+  //Actualizar comida
+  public updateFood(newFood:Food){
+    this.menu.forEach((food, index)=>{
+      if(food.id == newFood.id){
+        this.menu[index] = newFood;
+      }
+    });
+  };
+
+  //Eliminar comida
+  public deleteFood(deleteFood:Food){
+    this.menu.forEach((food, index)=>{
+      if(food.id == deleteFood.id){
+        this.menu.splice(index,1);
+      }
+    });
+  };
+
 }
